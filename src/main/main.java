a0ad6e0b65b4644817743077a086b447486b9600
MangoFrances <becerraladinosebastianjhon@gmail.com>
2025-03-06 09:09:39 -0500

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author becer
+ */
+public class main {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Palabras");
+        System.out.println("Mot");
+        System.out.println("Words");
+    }
+}
